343e8d32ccbf56e20ff7cc22677b5ec7578abfbf
m1k2i1t9ad <michaelt5593@gmail.com>
2024-12-21 20:46:36 +0300

    ...

--- storefront2/core/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-# Create your models here.
-
-# a model that extends abstract user:
-class User(AbstractUser): #creating this model is the 1st step to extend the user model
-    email=models.EmailField(unique=True)
-    
