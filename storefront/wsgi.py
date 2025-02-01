c0b67cee104b9dadcac561a28722ef8d7c864fbe
m1k2i1t9ad <michaelt5593@gmail.com>
2025-02-01 11:51:42 +0300

    update vercel.json and wsgi.py

--- storefront/wsgi.py
+++ storefront/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storefront.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storefront.settings.prod')
 
 application = get_wsgi_application()
 
